ecc17c8cd4be9d47dd73a4405241f1e0f6fb696f
zjx-immersion <jianxin_home@163.com>
2017-06-26 18:31:00 +0800

    [Jason]add a init spring boot api stack include some basic dependences

---
+++ src/main/java/com/tw/apistack/ApiStackApplication.java
@@ -0,0 +1,12 @@
+package com.tw.apistack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStackApplication.class, args);
+	}
+}
