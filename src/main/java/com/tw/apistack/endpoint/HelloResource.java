24539987edd689b267b31994e23b09913b0e47fa
zjx-immersion <jianxin_home@163.com>
2017-06-28 11:47:43 +0800

    [Jason]Added the check style in gradle customized plugin

--- src/main/java/com/tw/apistack/endpoint/HelloResource.java
+++ src/main/java/com/tw/apistack/endpoint/HelloResource.java
@@ -1,12 +1,11 @@
 package com.tw.apistack.endpoint;
 
-
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.tw.apistack.endpoint.dto.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.tw.apistack.endpoint.dto.Greeting;
 
 /**
  * Created by jxzhong on 2017/6/27.
@@ -15,12 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloResource {
 
-    private static final String template = "Hello, %s!";
+    private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+                String.format(TEMPLATE, name));
     }
 }
