5c07ac28867d7df1d1c5dec89f27f64d87d2429e
zjx-immersion <jianxin_home@163.com>
2017-07-04 18:56:57 +0800

    [Jason] fixed the clearMac.sh return error when contain is not exist

--- src/main/docker/clearMac.sh
+++ src/main/docker/clearMac.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker ps -a | grep zhongjx/api-stack  | awk "{print \$1}"
 
-docker rm -f $(docker ps -a | grep zhongjx/api-stack  | awk "{print \$1}")
+docker rm -f $(docker ps -a | grep zhongjx/api-stack  | awk "{print \$1}") 1>/dev/null 2>/dev/null
 
 docker images | grep zhongjx/api-stack | awk "{print \$3}"
 
-docker rmi -f $(docker images | grep zhongjx/api-stack | awk "{print \$3}" | sort -u)
+docker rmi -f $(docker images | grep zhongjx/api-stack | awk "{print \$3}" | sort -u) 1>/dev/null 2>/dev/null
 
 
