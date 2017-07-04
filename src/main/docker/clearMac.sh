f8865ee077aa5fc9292e7d2457b7f918c951eefe
zjx-immersion <jianxin_home@163.com>
2017-07-04 22:05:47 +0800

    [Jason] amended the script in clearMac.sh which can ignore the error exit 1

--- src/main/docker/clearMac.sh
+++ src/main/docker/clearMac.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker ps -a | grep zhongjx/api-stack  | awk "{print \$1}"
 
-docker rm -f $(docker ps -a | grep zhongjx/api-stack  | awk "{print \$1}") 1>/dev/null 2>/dev/null
+docker rm -f $(docker ps -a | grep zhongjx/api-stack  | awk "{print \$1}") 1>/dev/null 2>/dev/null ||true
 
 docker images | grep zhongjx/api-stack | awk "{print \$3}"
 
-docker rmi -f $(docker images | grep zhongjx/api-stack | awk "{print \$3}" | sort -u) 1>/dev/null 2>/dev/null
+docker rmi -f $(docker images | grep zhongjx/api-stack | awk "{print \$3}" | sort -u) 1>/dev/null 2>/dev/null || true
 
 
